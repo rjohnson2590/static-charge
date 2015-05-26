3c560520ed0cd52d04d41b62f8dbe864987762c6
Richard Johnson <rjohnson2590@gmail.com>
2015-05-26 15:46:56 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+fs.readdir(__dirname+'/../posts', function(error, directoryContents){
+	if (error){
+		throw new Error(error);
+	}
+	directoryContents.forEach(function(postFileName){
+		var postName= postFileName.replace(".jade", '');
+		router.get('/'+ postName, function(request, response){
+			response.render('../posts/'+ postFileName, {});
+		})
+	})
+})	
 
 module.exports = router;
